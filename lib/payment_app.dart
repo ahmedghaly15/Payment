8d364df8fb6bc1ff8047d8fc5456e8a8b435ff65
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-10 18:44:52 +0200

    Add getResponsiveText method, SizeConfig & Build PaymentApp

---
+++ lib/payment_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:payment/core/utils/colors_manager.dart';
+import 'package:payment/views/my_cart_view.dart';
+
+class PaymentApp extends StatelessWidget {
+  const PaymentApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Payment App',
+      theme: ThemeData(
+        primaryColor: ColorsManager.primaryColor,
+        scaffoldBackgroundColor: Colors.white,
+      ),
+      home: const MyCartView(),
+    );
+  }
+}
