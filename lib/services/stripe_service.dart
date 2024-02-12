b0aadd86c6ca448af0f040d3c41c3cf88816d7f1
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-12 22:03:11 +0200

    Finish createPaymentIntent method

---
+++ lib/services/stripe_service.dart
@@ -0,0 +1,22 @@
+import 'package:payment/core/utils/api_keys.dart';
+import 'package:payment/models/payment_intent_request_body.dart';
+import 'package:payment/models/payment_intent_response/payment_intent_response.dart';
+import 'package:payment/services/api_service.dart';
+
+class StripeService {
+  final ApiService _apiService = ApiService();
+
+  Future<PaymentIntentResponse> createPaymentIntent(
+    PaymentIntentRequestBody paymentIntentRequestBody,
+  ) async {
+    var response = await _apiService.post(
+      body: paymentIntentRequestBody.toJson(),
+      url: 'https://api.stripe.com/v1/payment_intents',
+      token: ApiKeys.secretKey,
+    );
+
+    final paymentIntent = PaymentIntentResponse.fromJson(response.data);
+
+    return paymentIntent;
+  }
+}
