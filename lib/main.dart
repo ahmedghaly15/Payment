a8eddd9074b743ce82e3fd13a9c6eb1b8d057ec2
Ahmed Ghaly <ahmedghaly0767@gmail.com>
2024-02-13 16:12:10 +0200

    Add publishableKey

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:payment/core/utils/api_keys.dart';
 import 'package:payment/payment_app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  Stripe.publishableKey = ApiKeys.publishableKey;
+
   runApp(const PaymentApp());
 }
--- lib/widgets/custom_primary_button_bloc_consumer.dart
+++ lib/widgets/custom_primary_button_bloc_consumer.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:payment/core/widgets/primary_button.dart';
 import 'package:payment/cubit/payment_cubit.dart';
+import 'package:payment/models/payment_intent_request_body.dart';
 import 'package:payment/views/thank_you_view.dart';
 
 class CustomPrimaryButtonBlocConsumer extends StatelessWidget {
@@ -30,7 +31,15 @@ class CustomPrimaryButtonBlocConsumer extends StatelessWidget {
         return PrimaryButton(
           isLoading: state is PaymentLoading ? true : false,
           buttonText: 'Continue',
-          onPressed: () {},
+          onPressed: () {
+            const PaymentIntentRequestBody paymentIntentRequestBody =
+                PaymentIntentRequestBody(
+              amount: '1000',
+              currency: 'USD',
+            );
+
+            context.read<PaymentCubit>().pay(paymentIntentRequestBody);
+          },
         );
       },
     );
